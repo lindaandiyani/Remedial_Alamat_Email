51b0522295b59da0528f8c7c18fbd312cf4760c2
lindaandiyani <lindaandiyani@gmail.com>
2019-11-27 14:48:54 +0700

    Alhamdulillah, semoga LULUS

---
+++ remidial_alamat_email.py
@@ -0,0 +1,75 @@
+# def cekNamaUser():
+# x= 'linda_andiyani@gma-il.com5'
+# print(x.split('-'))
+x= input('ketikkan alamat email (harus berformat NamaUser@Hosting.EKstensi): ')
+
+
+
+hurufAgkarakter= ['-','_','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','1','2','3','4','5','6','7','8','9','0']
+huruf = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+hurufAngka=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','1','2','3','4','5','6','7','8','9','0']
+for i in x:
+    if i == '@':
+        indeksUser = int(x.index(i))
+        # print(indeks)
+        # print(x.index(i))
+    if i == '.':
+        indeksHosting = int(x.index(i))
+        # print(indeksHosting)
+
+def cekNamaUser(x):
+    # email= False
+    for i in x[0:indeksUser]:
+        # print(i)
+        if i in hurufAgkarakter:
+            email= True
+            # print('email invalid')
+            # break
+        else: 
+            email = False
+            break
+    # print(email)
+    return email
+
+def cekHosting(x):
+    # email= False
+    for i in x[indeksUser+1:indeksHosting]:
+        # print(i)
+        if  i.lower() in hurufAngka:
+            email = True
+        else:
+            email= False
+            break
+    # print(email)
+    return email
+
+def cekEkstensi(x):
+    panjangekstensi = len(x[indeksHosting+1:])
+    # print(panjangekstensi)
+    for i in x[indeksHosting+1:]:
+        # print(i)
+        if i.lower() in huruf and panjangekstensi <= 5:
+            email = True
+        else:
+            email= False
+    # print(email)
+    return email
+# cekNamaUser(x)
+# cekHosting(x)
+# cekEkstensi(x)
+
+def cekEmail(x):
+    # semuaValid = False
+    cekUser = cekNamaUser(x)
+    cekHost = cekHosting(x)
+    cekEkst = cekEkstensi(x)
+    if cekUser and cekHost and cekEkst == True:
+        # semuaValid = True
+        print('Email  VALID')
+    else:
+        print('email TIDAK VALID')
+
+cekEmail(x)
+
+
+
